78c4546541c8cc060cb65bdf0447d113c552c4ff
Vol-Dem <saten898@gmail.com>
2026-08-31 22:05:01 +0300

    feat(a11y): respect reduced motion preferences

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { MotionConfig } from "framer-motion";
 import "./index.scss";
 import App from "./App";
 import ErrorBoundary from "./components/error-boundary/ErrorBoundary";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <MotionConfig reducedMotion="user">
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </MotionConfig>
   </React.StrictMode>,
 );
